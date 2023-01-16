c367a629a4059644b542b76e5ecd8557961717e7
Lee <yizhouli1822220@gmail.com>
2023-01-16 22:20:42 +0000

    add navbar and map components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="heading">
+      <h1>IFRC GO MAKE MAPS</h1>
+    </div>
+  )
+}
+
+export default Navbar
