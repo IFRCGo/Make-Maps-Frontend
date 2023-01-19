ce769b1c93ce73243b0d6bffcdfb352f52dbbed4
Lee <yizhouli1822220@gmail.com>
2023-01-19 23:36:17 +0000

    create the layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,47 @@
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import { Layout, theme } from 'antd';
 import Map from './components/Map';
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import { Grid } from '@mui/material';
+import FloatBut from './components/FloatBut';
+import HeaderContents from './components/HeaderContents';
 
+const { Header, Content, Footer } = Layout;
 
 function App() {
 
-  return (
-    <view>
-      <div 
-        className="App" 
-        style={{
-        display: "flex",
-        alignSelf: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: 20
-      }}>
-        <Grid container direction="column" alignItems="center" justify="center">
-          <h1>Yo</h1>
-          <Stack spacing={2} sx={{ width: 300, padding: 2}}>
-            <Autocomplete
-              id="free-solo-demo"
-              freeSolo
-              options={country.map((option) => option.title)}
-              renderInput={(params) => <TextField {...params} label="Country" />}
-            />
-          </Stack>
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
-          <Button variant="contained">Contained</Button>
-          {/* <Button variant="outlined">Outlined</Button> */}
-        </Grid>
-      </div>
-    </view>
+  return (
+    <Layout className="site-layout">
+      <Header
+        style={{
+          padding: 0,
+          background: colorBgContainer,
+        }}
+      >
+        <HeaderContents />
+      </Header>
+      <Content
+        style={{
+          margin: 0,
+          background: colorBgContainer,
+          height: '500px',
+        }}
+      >
+        <Map className="map-layout"/>
+        <FloatBut />
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        IFRC GO MAKE MAPS ©2023
+      </Footer>
+    </Layout>
   );
 }
-
-const country = [
-  { title: 'Paris', year: 1994 },
-  { title: 'London', year: 1972 },
-  { title: 'New York', year: 1974 },
-  { title: 'Tokyo', year: 2008 },
-  { title: 'Berlin', year: 1957 },
-  { title: 'Buenos Aires', year: 1993 },
-  { title: 'Cairo', year: 1994 },
-];
 
 export default App;
