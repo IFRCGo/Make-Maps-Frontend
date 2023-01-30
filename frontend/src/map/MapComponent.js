1312389f9a2db4ec0a035a61df0fa6e9b58a60f9
michaelsonH-ITDA <michelsonhsin@outlook.com>
2023-01-30 08:26:45 +0000

    Merge Completed

--- frontend/src/map/MapComponent.js
+++ frontend/src/map/MapComponent.js
@@ -50,7 +50,10 @@ const MapComponent = () => {
         }}
         onClick={handleMapClick}
         style={{ width: "100%", height: " calc(100vh - 94px)" }}
-        mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC"
+        // mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC" // Basic layer
+        // mapStyle="https://api.maptiler.com/maps/streets-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC" // Street Layer
+        // mapStyle="https://api.maptiler.com/maps/openstreetmap/style.json?key=HMeYX3yPwK7wfZQDqdeC" // open street layer
+        mapStyle="https://api.maptiler.com/maps/hybrid/style.json?key=HMeYX3yPwK7wfZQDqdeC"  // satellite layer
       >
         <NavigationControl position="top-left" />
         {pins.map((pin, index) => (
