04f37eb48e01d3e2eb4639c19a735261bde9a587
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-02-02 19:36:17 +0000

    :sparkles: Added Painting Functionality

--- frontend/src/map/ToolBar.js
+++ frontend/src/map/ToolBar.js
@@ -2,142 +2,154 @@ import React from "react";
 import { Drawer, FloatButton, Button, Space } from "antd";
 import { useState } from "react";
 import {
-	ToolOutlined,
-	PushpinOutlined,
-	FormOutlined,
-	LinkOutlined,
-	DownloadOutlined,
+  ToolOutlined,
+  PushpinOutlined,
+  FormOutlined,
+  LinkOutlined,
+  DownloadOutlined,
+  FormatPainterOutlined,
 } from "@ant-design/icons";
 import "./ToolBar.css";
 
-const ToolBar = ({ handlePinButton, handleTextButton, props, setMapType }) => {
-	const [open, setOpen] = useState(true);
-	const [visual, setVisual] = useState(true);
-	const showDrawer = () => {
-		open === false ? setOpen(true) : setOpen(false);
-		visual === false
-			? setVisual(true)
-			: setTimeout(() => {
-					setVisual(false);
-			  }, 300);
-	};
-	const onClose = () => {
-		setOpen(false);
-		setVisual(false);
-	};
+const ToolBar = ({
+  handlePinButton,
+  handleTextButton,
+  handlePaintButton,
+  props,
+  setMapType,
+}) => {
+  const [open, setOpen] = useState(true);
+  const [visual, setVisual] = useState(true);
+  const showDrawer = () => {
+    open === false ? setOpen(true) : setOpen(false);
+    visual === false
+      ? setVisual(true)
+      : setTimeout(() => {
+          setVisual(false);
+        }, 300);
+  };
+  const onClose = () => {
+    setOpen(false);
+    setVisual(false);
+  };
 
-	const changeMap = (event) => {};
+  const changeMap = (event) => {};
 
-	return (
-		<>
-			<FloatButton
-				shape="square"
-				style={{ right: 24, marginBottom: 20 }}
-				icon={<ToolOutlined />}
-				onClick={showDrawer}
-			/>
-			<div
-				className="tool-area"
-				style={{ display: visual === false ? "none" : "initial" }}
-			>
-				<Drawer
-					autoFocus={false}
-					height={100}
-					mask={false}
-					placement="bottom"
-					closable={false}
-					onClose={onClose}
-					open={open}
-					getContainer={false}
-				>
-					<div className="tool-button">
-						<Space>
-							<Button
-								type="text"
-								size="large"
-								icon={<PushpinOutlined onClick={handlePinButton} />}
-							/>
-							<Button
-								type="text"
-								size="large"
-								icon={<FormOutlined onClick={handleTextButton} />}
-							/>
-							<Button type="text" size="large" icon={<LinkOutlined />} />
-							<Button type="text" size="large" icon={<DownloadOutlined />} />
-							<Button
-								type="text"
-								size="large"
-								icon={
-									<img
-										src="https://cloud.maptiler.com/static/img/maps/basic-v2.png?t=1663665773"
-										onClick={() =>
-											setMapType(
-												"https://api.maptiler.com/maps/basic-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC"
-											)
-										}
-										width="40"
-										height="40"
-										alt="basic"
-									/>
-								}
-							/>
-							<Button
-								type="text"
-								size="large"
-								icon={
-									<img
-										src="https://cloud.maptiler.com/static/img/maps/streets-v2.png?t=1663665773"
-										onClick={() =>
-											setMapType(
-												"https://api.maptiler.com/maps/streets-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC"
-											)
-										}
-										width="40"
-										height="40"
-										alt="basic"
-									/>
-								}
-							/>
-							<Button
-								type="text"
-								size="large"
-								icon={
-									<img
-										src="https://cloud.maptiler.com/static/img/maps/openstreetmap.png?t=1663665773"
-										onClick={() =>
-											setMapType(
-												"https://api.maptiler.com/maps/openstreetmap/style.json?key=HMeYX3yPwK7wfZQDqdeC"
-											)
-										}
-										width="40"
-										height="40"
-										alt="basic"
-									/>
-								}
-							/>
-							<Button
-								type="text"
-								size="large"
-								icon={
-									<img
-										src="https://cloud.maptiler.com/static/img/maps/hybrid.png?t=1663665773"
-										onClick={() =>
-											setMapType(
-												"https://api.maptiler.com/maps/hybrid/style.json?key=HMeYX3yPwK7wfZQDqdeC"
-											)
-										}
-										width="40"
-										height="40"
-										alt="basic"
-									/>
-								}
-							/>
-						</Space>
-					</div>
-				</Drawer>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <FloatButton
+        shape="square"
+        style={{ right: 24, marginBottom: 20 }}
+        icon={<ToolOutlined />}
+        onClick={showDrawer}
+      />
+      <div
+        className="tool-area"
+        style={{ display: visual === false ? "none" : "initial" }}
+      >
+        <Drawer
+          autoFocus={false}
+          height={100}
+          mask={false}
+          placement="bottom"
+          closable={false}
+          onClose={onClose}
+          open={open}
+          getContainer={false}
+        >
+          <div className="tool-button">
+            <Space>
+              <Button
+                type="text"
+                size="large"
+                icon={<PushpinOutlined onClick={handlePinButton} />}
+              />
+              <Button
+                type="text"
+                size="large"
+                icon={<FormOutlined onClick={handleTextButton} />}
+              />
+              <Button
+                type="text"
+                size="large"
+                icon={<FormatPainterOutlined onClick={handlePaintButton} />}
+              />
+              <Button type="text" size="large" icon={<LinkOutlined />} />
+              <Button type="text" size="large" icon={<DownloadOutlined />} />
+              <Button
+                type="text"
+                size="large"
+                icon={
+                  <img
+                    src="https://cloud.maptiler.com/static/img/maps/basic-v2.png?t=1663665773"
+                    onClick={() =>
+                      setMapType(
+                        "https://api.maptiler.com/maps/basic-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC"
+                      )
+                    }
+                    width="40"
+                    height="40"
+                    alt="basic"
+                  />
+                }
+              />
+              <Button
+                type="text"
+                size="large"
+                icon={
+                  <img
+                    src="https://cloud.maptiler.com/static/img/maps/streets-v2.png?t=1663665773"
+                    onClick={() =>
+                      setMapType(
+                        "https://api.maptiler.com/maps/streets-v2/style.json?key=HMeYX3yPwK7wfZQDqdeC"
+                      )
+                    }
+                    width="40"
+                    height="40"
+                    alt="basic"
+                  />
+                }
+              />
+              <Button
+                type="text"
+                size="large"
+                icon={
+                  <img
+                    src="https://cloud.maptiler.com/static/img/maps/openstreetmap.png?t=1663665773"
+                    onClick={() =>
+                      setMapType(
+                        "https://api.maptiler.com/maps/openstreetmap/style.json?key=HMeYX3yPwK7wfZQDqdeC"
+                      )
+                    }
+                    width="40"
+                    height="40"
+                    alt="basic"
+                  />
+                }
+              />
+              <Button
+                type="text"
+                size="large"
+                icon={
+                  <img
+                    src="https://cloud.maptiler.com/static/img/maps/hybrid.png?t=1663665773"
+                    onClick={() =>
+                      setMapType(
+                        "https://api.maptiler.com/maps/hybrid/style.json?key=HMeYX3yPwK7wfZQDqdeC"
+                      )
+                    }
+                    width="40"
+                    height="40"
+                    alt="basic"
+                  />
+                }
+              />
+            </Space>
+          </div>
+        </Drawer>
+      </div>
+    </>
+  );
 };
 
 export default ToolBar;
