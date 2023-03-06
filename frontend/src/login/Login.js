5bdfbf732903b7f6658a1b27c01d3def3ed707d0
Danny <danielswarup1@gmail.com>
2023-03-06 00:58:33 +0000

    feat: login & authentication

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,15 +1,44 @@
-import React from "react";
-import {
-	UserOutlined,
-	EyeInvisibleOutlined,
-	EyeTwoTone,
-	LockOutlined,
-} from "@ant-design/icons";
+import React, { useState } from "react";
+import * as msal from "@azure/msal-browser";
+
 import { Input, Space, Button } from "antd";
 import GoLogo from "../images/goLogo.svg";
 
 const Login = () => {
 	const [passwordVis, setPasswordVis] = React.useState(false);
+
+	const msalConfig = {
+		auth: {
+			clientId: process.env.REACT_APP_CLIENT_ID,
+			authority: process.env.REACT_APP_LOGIN_AUTHORITY,
+			redirectUri: process.env.REACT_APP_REDIRECT_URI,
+		},
+	};
+	const scopes = ["openid", "profile", "email"]; // add any additional scopes here
+
+		const [loggedIn, setLoggedIn] = useState(false);
+		const [user, setUser] = useState(null);
+
+		const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+		const handleLogin = async () => {
+			try {
+				const loginResponse = await msalInstance.loginPopup({
+					scopes,
+				});
+
+				setUser(loginResponse.account);
+				//console.log(loginResponse.account.username) //This gets their email address
+				setLoggedIn(true);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+	const handleLogout = async () => {
+		await msalInstance.logoutPopup();
+		setLoggedIn(false);
+		setUser(null);
+	};
 
 	return (
 		<>
@@ -28,24 +57,20 @@ const Login = () => {
 				}}
 			>
 				<img src={GoLogo} alt="logo" className="logo" />
-				<Input
-					size="large"
-					placeholder="username"
-					prefix={<UserOutlined />}
-					style={{ marginTop: 20 }}
-				/>
-				<Input.Password
-					size="large"
-					placeholder="password"
-					prefix={<LockOutlined />}
-					iconRender={(visible) =>
-						visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-					}
-					style={{ marginTop: 20 }}
-				/>
-				<Button type="primary" danger style={{ marginTop: 20, width: "auto" }}>
-					Login
-				</Button>
+				{loggedIn ? (
+					<>
+						<p>Signed in as {user.name}</p>
+
+						<Button type="primary" danger onClick={handleLogout} style={{ marginTop: 20, width: "auto" }}>
+							Logout
+						</Button>
+					</>
+				) : (
+					<Button type="primary" danger onClick={handleLogin} style={{ marginTop: 20, width: "auto" }}>
+						Login
+					</Button>
+				)}
+
 			</Space>
 		</>
 	);
