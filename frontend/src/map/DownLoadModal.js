946ec51c688ea8f67205651c76b1fc910f9972bd
Lee <yizhouli1822220@gmail.com>
2023-03-15 00:04:46 +0000

    :zap: Change the UI of download modal

--- frontend/src/map/DownLoadModal.js
+++ frontend/src/map/DownLoadModal.js
@@ -1,35 +1,43 @@
-import React, { useState } from "react";
-import { Modal, Button, Space } from "antd";
+import React from "react";
+import { Modal, Button } from "antd";
 
 const DownLoadModal = ({
-    isDownloadModalOpen, setIsDownloadModalOpen, download
+  isDownloadModalOpen,
+  setIsDownloadModalOpen,
+  download,
 }) => {
-    const handleCancel = () => {
-        setIsDownloadModalOpen(false);
-    };
+  const handleCancel = () => {
+    setIsDownloadModalOpen(false);
+  };
 
-    const doNothing = () => {
-        
-    }
-    return (
-        <>
-            <Modal
-                title="Share Link"
-                centered
-                open={isDownloadModalOpen}
-                onCancel={handleCancel}
-                footer={[
-                    <Button key="pdf" onClick={() => download("PDF")}>
-                        Download PDF
-                    </Button>,
-                    <Button key="png" onClick={() => download("PNG")}>
-                        Download PNG
-                    </Button>,
-                ]}
-            >
-            </Modal>
-        </>
-    );
-}
+  return (
+    <>
+      <Modal
+        title="Download"
+        centered
+        open={isDownloadModalOpen}
+        onCancel={handleCancel}
+        footer={[
+          <Button
+            style={{ width: "auto" }}
+            danger
+            key="pdf"
+            onClick={() => download("PDF")}
+          >
+            Download PDF
+          </Button>,
+          <Button
+            style={{ width: "auto" }}
+            danger
+            key="png"
+            onClick={() => download("PNG")}
+          >
+            Download PNG
+          </Button>,
+        ]}
+      ></Modal>
+    </>
+  );
+};
 
 export default DownLoadModal;
